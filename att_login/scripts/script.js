d1e714b0fb71981fb7ab9f9e11b151c266b9b51d
André Felipe <160754000+itsandrezl@users.noreply.github.com>
2024-04-22 12:08:36 -0300

    Add files via upload

---
+++ att_login/scripts/script.js
@@ -0,0 +1,17 @@
+function salvarDados(){
+
+    var nome = document.getElementById('nome').value;
+    var senha = document.getElementById('senha').value;
+
+
+    var login ={
+        nome: nome,
+        senha: senha,
+    }
+
+    console.log(login)
+    console.log(nome)
+    console.log(senha)
+
+
+}
